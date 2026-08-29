8330622b1ff2aeb4d0f1d745b18ff5a1f1ad8593
Yana1462110 <yana.heyvaert2110@gmail.com>
2026-08-29 16:44:42 +0200

    Add icon files to cache in service worker
    Added icons to the cache for offline access.

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,29 @@
 const CACHE_NAME = "soundboard-v1";
 
 const FILES_TO_CACHE = [
-    "./",
-    "./index.html",
-    "./style.css",
-    "./script.js",
-    "./manifest.json",
-    "./sounds/hallo.mp3",
-    "./sounds/nee.mp3"
+"./",
+"./index.html",
+"./style.css",
+"./script.js",
+"./manifest.json",
+"./hallo.mp3",
+"./nee.mp3",
+"./icon-192.png",
+"./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
+event.waitUntil(
+caches.open(CACHE_NAME).then(cache => {
+return cache.addAll(FILES_TO_CACHE);
+})
+);
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+event.respondWith(
+caches.match(event.request).then(response => {
+return response || fetch(event.request);
+})
+);
 });
